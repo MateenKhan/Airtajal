981ea90562bff91783d62daa2a33022454677ed4
Mateen <makjavaprogrammer@gmail.com>
2024-12-21 16:57:36 +0530

    removed all code

--- client/src/Readme.md
+++
@@ -1,5 +0,0 @@
-Application to assist Interior, Carpenter and welders by providing below features
-
-1. Price esitmation
-2. Project planning
-3. Customer-project relation managment 
--- client/src/app/estimate/models/_1d.ts
+++
@@ -1,8 +0,0 @@
-export class _1d {
-    public x!: number;
-    public y!: number;
-    public thikness!: number;
-    public area!: number;
-    public label = "";
-    constructor() { }
-}
