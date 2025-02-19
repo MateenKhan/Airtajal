51ebd9b15863e65afea32449f69c141ca5f81491
Mateen <makjavaprogrammer@gmail.com>
2025-02-19 21:37:53 +0530

    updated signup router

--- Ui/src/app/signup/signup.component.ts
+++ Ui/src/app/signup/signup.component.ts
@@ -8,13 +8,14 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
-  imports: [MatGridListModule, MatDividerModule, MatButtonModule, MatCardModule, MatChipsModule, MatProgressBarModule, MatFormFieldModule, MatIconModule, MatInputModule],
+  imports: [RouterModule, MatGridListModule, MatDividerModule, MatButtonModule, MatCardModule, MatChipsModule, MatProgressBarModule, MatFormFieldModule, MatIconModule, MatInputModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 
 
@@ -22,11 +23,18 @@ import {MatGridListModule} from '@angular/material/grid-list';
 export class SignupComponent {
   hide = signal(true);
 
+  constructor(private router:RouterModule){}
+
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
+
   longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
   Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
   usually kept as a companion animal or for showing.`;
+
+  login(){
+    // this.router.
+  }
 }
